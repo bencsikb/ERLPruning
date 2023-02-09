67c8508d5cdd25bfce7ad4c004d33fd65db2ff2a
bencsikb <bencsik.blanka@gmail.com>
2023-02-09 19:25:17 +0100

    make df in a different function, last pruned layer info added

--- data_statistics.py
+++ data_statistics.py
@@ -17,30 +17,14 @@ def create_hist(data, bins,  title, xlabel, ylabel):
     plt.ylabel(ylabel)
 
 
-def spn_label_distrubution(datapath, savepath, binwidth):
+def spn_label_distrubution(samples_df, savepath, binwidth):
     """
-    Create a histogram of the data labels (dperf, sparsity) with given bins.
-    :param datapath: label folder
+    Creates a histogram of the data labels (dperf, sparsity) with given bins and saves the figures.
+    :param samples_df: summary dataframe
     :param savepath: path to the folder where the resulted figures should be saved
     :param binwidth: bin width
     :return:
     """
-
-    samples = os.listdir(datapath)
-    samples_df = pd.DataFrame(columns=["spars", "dmap", "drec", "dprec", "file"])
-
-    for sample in samples:
-        with open(os.path.join(datapath, sample), "r") as f:
-            spars, dmap, drec, dprec = f.read().split(" ")
-            samples_df.loc[len(samples_df)] = [spars, dmap, drec, dprec, sample]
-
-        #df = pd.read_csv(os.path.join(datapath, sample), sep=" ")
-        #df[sample] = 0
-        #samples_df.loc[len(samples_df)] = df.columns
-
-        convert_dict = {'spars': float, 'dmap': float, 'drec':float,  'dprec': float, 'file': str}
-        samples_df = samples_df.astype(convert_dict)
-
 
     bins = np.arange(0, 1, binwidth)
     create_hist(denormalize(samples_df['spars'].tolist(), 0 ,1), bins, "Distribution of Label: sparsity", "sparsity", "number of samples" )
@@ -50,23 +34,37 @@ def spn_label_distrubution(datapath, savepath, binwidth):
     plt.savefig(os.path.join(savepath, "label_dmap_distribution.png"))
 
 
-def spn_label_distrubution_wcondition(datapath, savepath, binwidth, threshold):
+def create_summary_dataframe(labelspath): #, savepath, binwidth, threshold):
+    """
+    Creates a summary datframe of the samples.
+    :param labelspath: path to the labels folder
+    :return: dataframe with the following information:
+             "spars", "dmap", "drec", "dprec", "file", "last pruned layer"
     """
 
-    :param datapath:
-    :param savepath:
-    :param binwidth:
-    :param threshold:
-    :return:
-    """
+    state_names = ['alpha', 'in_ch', 'out_ch', 'kernel', 'stride', 'pad', 'spars']
+    samples_df = pd.DataFrame(columns=["spars", "dmap", "drec", "dprec", "file", "last pruned layer"])
 
-    state_samples = os.listdir(datapath)
+    samples = os.listdir(labelspath)
 
-    for state_sample in state_samples:
-        print()
+    for sample in samples:
+        with open(os.path.join(labelspath, sample), "r") as f:
+            spars, dmap, drec, dprec = f.read().split(" ")
 
+        """ This way the dataframe doesn't contain the data to the first layer, but we don't really care about 
+               that in this task."""
+        state_df = pd.read_csv(os.path.join(labelspath.replace("labels","states"), sample), sep=" ", names=state_names)
 
-        
+        # Get the fist row where kernel is not -1
+        last_pruned_layer = state_df[(state_df.kernel > -1.0)].index[-1] + 1
+
+        samples_df.loc[len(samples_df)] = [spars, dmap, drec, dprec, sample, last_pruned_layer]
+
+    convert_dict = {'spars': float, 'dmap': float, 'drec': float, 'dprec': float, 'file': str, 'last pruned layer': int}
+    samples_df = samples_df.astype(convert_dict)
+    print(samples_df)
+
+    return samples_df
 
 
 
@@ -74,4 +72,6 @@ if __name__ == '__main__':
 
     datapath = "/data/blanka/DATASETS/SPN/all4/labels"
     path = "/home/blanka/ERLPruning/sandbox"
-    spn_label_distrubution(datapath, path, binwidth=0.1)
+
+    samples_df = create_summary_dataframe(datapath)
+    spn_label_distrubution(samples_df, path, binwidth=0.1)
