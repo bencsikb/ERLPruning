7a436a595e1ffa87c2f3daddf1da616459bb1199
bencsikb <bencsik.blanka@gmail.com>
2023-02-19 12:38:03 +0100

    dmap distribution histogram above a given layer func added

--- data_statistics.py
+++ data_statistics.py
@@ -66,12 +66,55 @@ def create_summary_dataframe(labelspath): #, savepath, binwidth, threshold):
 
     return samples_df
 
+def condition1(samples_df, dmap_thresh1, dmap_thresh2, layer_thresh):
+    """
+    Counts the samples where the index of last pruned (prunable) layer is greater than layer_thresh and
+    dmap is between dmap_thresh1 and dmap_thresh2.
+    :param samples_df: summary dataframe
+    """
+
+    #rows = samples_df[samples_df['dmap']]
+    cnt = len(samples_df[(samples_df['last pruned layer']>layer_thresh) & (samples_df['dmap']<dmap_thresh2) & (samples_df['dmap']>dmap_thresh1)])
+
+    print(f"Number of samples satisfying the condition: {cnt}")
+    return cnt
+
+def hist_w_condition(samples_df, layer_thresh, savepath):
+    """
+    Creates a histogram of the dmaps above a given layer (layer_thresh).
+    :param samples_df:
+    :param layer_thresh:
+    :param savepath:
+    :return:
+    """
+
+    bins = np.arange(-1, 1, 0.2)
+    values = np.zeros(bins.shape[0]-1)
+    print(bins.shape, values.shape)
+    for i, bl in enumerate(bins[:-1]):
+        ul = bins[i+1]
+        values[i] = condition1(samples_df, bl, ul, layer_thresh)
+
+    print(values)
+    #create_hist(bins.shape[0], values, f"dmap distribution above layer {layer_thresh}", "dmap", "number of samples")
+    plt.stem(range(values.shape[0]), values)
+    plt.title(f"dmap distribution above layer {layer_thresh}")
+    plt.xlabel("dmap")
+    plt.ylabel("number of samples")
+    plt.savefig(os.path.join(savepath, f"dmap_distribution_layer_{layer_thresh}.png"))
 
 
 if __name__ == '__main__':
 
-    datapath = "/data/blanka/DATASETS/SPN/all4/labels"
+    datapath = "/data/blanka/DATASETS/SPN/all5/labels"
     path = "/home/blanka/ERLPruning/sandbox"
+    df_read = True
 
-    samples_df = create_summary_dataframe(datapath)
-    spn_label_distrubution(samples_df, path, binwidth=0.1)
+    if df_read:
+        samples_df = pd.read_csv("/home/blanka/ERLPruning/sandbox/samples_df.csv")
+    else:
+        samples_df = create_summary_dataframe(datapath)
+        samples_df.to_csv(os.path.join(path, "samples_df.csv"))
+
+    #spn_label_distrubution(samples_df, path, binwidth=0.1)
+    hist_w_condition(samples_df, 80, path)
