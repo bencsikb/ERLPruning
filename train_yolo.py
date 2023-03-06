6854b7521c51d1f382a98a0895dd722ae10fd1f6
bencsikb <bencsik.blanka@gmail.com>
2023-03-06 17:53:48 +0100

    train from scratch with higher lr

--- train_yolo.py
+++ train_yolo.py
@@ -116,7 +116,7 @@ def train(model, optimizer, scheduler, hyp, opt, device, dataset, dataloader, da
 
             # print("Targets in train ", targets)
 
-            """todo
+
             # Warmup
             if ni <= nw:
                 xi = [0, nw]  # x interp
@@ -127,7 +127,7 @@ def train(model, optimizer, scheduler, hyp, opt, device, dataset, dataloader, da
                     x['lr'] = np.interp(ni, xi, [0.1 if j == 2 else 0.0, x['initial_lr'] * lf(epoch)])
                     if 'momentum' in x:
                         x['momentum'] = np.interp(ni, xi, [0.9, hyp['momentum']])
-            """
+
 
             # Multi-scale
             if opt.multi_scale:
@@ -300,7 +300,7 @@ if __name__ == '__main__':
     # For yolo training
     parser.add_argument('--weights', type=str, default="/home/blanka/ERLPruning/weights/yolov4.weights.1", help='initial weights path')
     #parser.add_argument('--weights', default= "/data/blanka/ERLPruning/runs/YOLOv4_KITTI/exp_kitti_tvt/weights/best.pt") #pretrained
-    #parser.add_argument('--weights', default= "/data/blanka/ERLPruning/runs/YOLOv4_PascalVoc/exp_fromscratch_7__repr/weights/last.pt") #pretrained
+    #parser.add_argument('--weights', default= "/data/blanka/ERLPruning/runs/YOLOv4_PascalVoc/exp_fromscratch_7_repr_resume/weights/last.pt") #pretrained
     parser.add_argument('--cfg', type=str, default='cfg/yolov4_pascalvoc.cfg', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/pascalvoc.yaml', help='data.yaml path')
     parser.add_argument('--batch_size', default=16)
@@ -328,12 +328,12 @@ if __name__ == '__main__':
     parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
     parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
     parser.add_argument('--names', type=str, default='data/kitti.names', help='*.cfg path')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--augme nt', action='store_true', help='augmented inference')
     parser.add_argument('--merge', action='store_true', help='use Merge NMS')
     parser.add_argument('--verbose', action='store_true', help='report mAP by class')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--prune_flag', type=bool, default=False)
-    parser.add_argument('--test_case', default="deleteme")
+    parser.add_argument('--test_case', default="exp_fromscratch_8")
 
     parser.add_argument('--yolo_layers', default=[138, 148, 149, 160])
     parser.add_argument('--alpha_seq', default=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.1,0.1,0,0,1.9,0,0,2.2,0.1,2.2,0.3,1.2,0.1,0.1,0,0,0,0,0.1,0.5,0.1,2.1,1.8,2.2], help="RL predicted policy")
