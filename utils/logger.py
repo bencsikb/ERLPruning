9337524dba39fabff27cea54876d2bf0f243c514
Lisa <mona_lisa@gmail.com>
2023-11-13 15:46:17 +0000

    do not overwrite existing test case

--- utils/logger.py
+++ utils/logger.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 from varname import nameof
 from torch.utils.tensorboard import SummaryWriter
+from utils.config_parser import ConfigParser
 
 
 class BasicLogger():
@@ -14,14 +15,24 @@ class BasicLogger():
 
         self.log_dir = os.path.join(log_dir, test_case)
         self.test_case = test_case
-        if not os.path.exists(self.log_dir): os.mkdir(self.log_dir)
+        
+        # Do not overwrite existing test case
+        while os.path.exists(self.log_dir):
+            self.log_dir += '_cont'
+        
+        os.mkdir(self.log_dir)
+           
 
-    def log_settings(self, opt, settings_dict=None):
-        path = os.path.join(self.log_dir, "settings.txt")
+    def log_settings(self, conf, settings_dict=None):
+        # TODO this should be a method in ConfigPrser
+        path = os.path.join(conf.paths.log_dir, conf.logging.folder, conf.dynamic.test_case, "settings.txt")
+        ConfigParser.save(conf, path)
+        """
         with open(path, 'w') as file:
             json.dump(opt.__dict__, file, indent=2)
             file.write("\n")
             file.write(json.dumps(settings_dict))
+        """
 
     def log_model(self, model):
         path = os.path.join(self.log_dir, "model.txt")
